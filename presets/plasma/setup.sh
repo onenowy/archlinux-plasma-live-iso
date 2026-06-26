4c0044aaeaa16095183382ad10cfc3c9cd420087
onenowy <onenowy@gmail.com>
2026-06-26 16:30:42 +0900

    merge custom to plasma

--- presets/plasma/setup.sh
+++ presets/plasma/setup.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-# Plasma preset - KDE Plasma desktop environment
+# Plasma preset - KDE Plasma with zsh, starship
 
 source "$REPO_DIR/scripts/setup_functions.sh"
 
 setup_desktop_env
 setup_bluetooth
 setup_kde_configs
+setup_zsh_starship
 setup_user
